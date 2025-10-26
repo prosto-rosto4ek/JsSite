1099aa11fe4ae7f645fe5c42f518e7671a75552f
prosto-rosto4ek <profikprofik2006@gmail.com>
2025-10-26 17:42:59 +0300

    test to create file in new folder

---
+++ backend/htmlGenerater.js
@@ -0,0 +1,32 @@
+function createElementToEnd(typeElement, parent,id=null){//Добавляет элемент(первый параметр) в КОНЕЦ родительского(второго аргумента)
+    createdEl=document.createElement(typeElement);
+    parent.appendChild(createdEl);
+       if(id){
+        createdEl.id=id;
+        }
+    return createdEl;
+}
+function createElementToStart(typeElement, parent,id=null){//Добавляет элемент(первый параметр) в НАЧАЛО родительского(второго аргумента)
+    createdEl=document.createElement(typeElement);
+    parent.prepend(createdEl);
+       if(id){
+        createdEl.id=id;
+        }
+    return createdEl;
+}
+
+
+function addCSSClass(elementID,className){//Добавление по айди(первый параметр) css-класса (второй параметр)
+    let element = document.getElementById(elementID);
+    element.classList.add(className);
+}
+function removeCSSClass(elementID,className){//Удаление по айди(первый параметр) css-класса (второй параметр)
+    let element = document.getElementById(elementID);
+    element.classList.remove(className);
+}
+
+
+function setContent(elementID,content){
+    let element = document.getElementById(elementID);
+    element.content = content;
+}
